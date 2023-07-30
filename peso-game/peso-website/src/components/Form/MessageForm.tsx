e2602fb0607fa6ac994a692114a46d07cc911108
DenisFedash <0501054104d@gmail.com>
2023-07-30 20:32:29 +0300

    add captcha

--- peso-game/peso-website/src/components/Form/MessageForm.tsx
+++ peso-game/peso-website/src/components/Form/MessageForm.tsx
@@ -1,127 +1,14 @@
 "use client";
-import * as s from "./form.module.scss";
+
 import React, { FC } from "react";
-import { useState } from "react";
-import Image from "next/image";
-import profilePic from "../../../assets/img/home/desktop/enquete-captcha-voorbeeld.jpg";
 import {
   FormData,
   LanguageFCComponentsProps,
 } from "@/interfaces/Props.interface";
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import ReCAPTCHA from "react-google-recaptcha";
 import localFont from "next/font/local";
 import useTranslation from "@/app/i18n/client";
-
-// import axios from "axios";
-
-// const initialValues: FormData = {
-//   name: "",
-//   email: "",
-//   message: "",
-// };
-
-// export const Form: FC = () => {
-//   const [formData, setFormData] = useState(initialValues);
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setFormData((prevFormData) => ({
-//       ...prevFormData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-//     const feedBackData = { formData };
-//     console.log(feedBackData);
-
-//     // запрос для добавления отзыва в базу данных, адрес пока условный,
-//     //  так как бека еще нет, потом поменям в случае чего
-//     // пока закомментировала, чтоб ошибку в консоль не било
-
-//     // const addFeedback = async()=>{
-//     //   try {
-//     //     await axios.post('http://localhost:3001/api/feedback/add', feedBackData)
-//     //   } catch (err) {
-//     //     console.log(err)
-//     //   }
-//     // }
-//     // addFeedback();
-
-//     reset();
-//   };
-
-//   const reset = () => {
-//     setFormData({ name: "", email: "", message: "" });
-//   };
-
-//   return (
-//     <div className="">
-//       <h2 className="">FeedBack Form</h2>
-//       <p className="">
-//         The video shows one guild attacking another guild. Enjoy the gameplay
-//         right now.
-//       </p>
-
-//       <p className="">Drop Us a Line</p>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           className=""
-//           type="text"
-//           placeholder="Your name & nickname.."
-//           name="name"
-//           pattern="^[a-zA-Zа-яА-Я0-9_-]{3,16}$"
-//           title="Name may contain only letters, apostrophe, dash and spaces, 3-16 symbols. "
-//           required
-//           value={name}
-//           onChange={handleChange}
-//         />
-
-//         <input
-//           className=""
-//           type="email"
-//           placeholder="Your email.."
-//           name="email"
-//           pattern="^([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})@([A-Za-z]{1,}\.){1,2}[a-z]{2,4}$"
-//           title="The part of the email with the username can contain Latin letters, numbers, dot, hyphen, underscore."
-//           required
-//           value={email}
-//           onChange={handleChange}
-//         />
-
-//         <input
-//           className=""
-//           type="text"
-//           placeholder="Your message & question.."
-//           name="message"
-//           pattern="^[0-9A-Za-zА-Яа-яЁё]{10,200}*$"
-//           title="10 to 200 symbols, numbers and letters"
-//           required
-//           value={message}
-//           onChange={handleChange}
-//         />
-
-//         <div className="">
-//           <Image src={profilePic} alt="Capcha" className="" />
-
-//           <button type="submit" className="">
-//             <div className="">
-//               <p className="">Send Message</p>
-//               <ul className="">
-//                 <li className=""></li>
-//                 <li className=""></li>
-//                 <li className=""></li>
-//               </ul>
-//             </div>
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
 
 const myFont = localFont({
   src: "../../../public/MyFont-Regular.otf",
@@ -139,7 +26,6 @@ export const MessageForm: FC<LanguageFCComponentsProps> = ({ lng }) => {
   const { t } = useTranslation(lng, "main");
 
   const handleSubmit = (values: FormData) => {
-    // e.preventDefault();
     console.log(values);
   };
 
@@ -235,15 +121,15 @@ export const MessageForm: FC<LanguageFCComponentsProps> = ({ lng }) => {
                 />
               </div>
               <div className="mt-[90px] flex justify-center items-center flex-col md:flex-row-reverse md:m-auto md:mt-[70px] lg:mt-[80px]">
-                {/* <ReCAPTCHA
-                  sitekey="6LcAEmYnAAAAALf6zXE81rddyCfJ99W05t5i3ZxL"
+                <ReCAPTCHA
+                  sitekey="6Lcc1WknAAAAAHxRlLXuD8WhkOXTlR31zYR1XU7m"
                   onChange={(value: any) => console.log("recaptcha", value)}
                 />
                 <ErrorMessage
                   name="recaptcha"
                   component="div"
                   className="error"
-                /> */}
+                />
 
                 <button
                   type="submit"
