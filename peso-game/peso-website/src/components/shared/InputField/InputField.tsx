5d154afa9cf4174767731d8fbcdea2eb35c475b1
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2023-09-06 18:51:09 +0300

    sign in section done

---
+++ peso-game/peso-website/src/components/shared/InputField/InputField.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { ErrorMessage } from "formik";
+
+interface InputFieldsProps {
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  errors?: string;
+  touched: boolean | string | undefined;
+}
+
+const InputField: FC<InputFieldsProps> = ({
+  name,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+  errors,
+  touched,
+}) => (
+  <label htmlFor={name}>
+    <input
+      type="text"
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+      className={`border-b-2 ${
+        errors && touched ? "border-error-color" : "border-main-font-color"
+      } appearance-none bg-transparent py-2 px-2 w-full text-gray-700 mr-3 leading-tight focus:outline-none`}
+    />
+    <ErrorMessage
+      component="div"
+      name={name}
+      className="mt-2 text-error-color"
+    />
+  </label>
+);
+
+export default InputField;
---
+++ peso-game/peso-website/src/components/utils/validation/registervalid.js
@@ -0,0 +1,32 @@
+import * as Yup from "yup";
+
+export const RegisterValidationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required("Name is required")
+    .email("Please enter a valid name")
+    .max(254, "Max 254 characters"),
+  email: Yup.string()
+    .required("Email is required")
+    .email("Please enter a valid email")
+    .max(254, "Max 254 characters"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters"),
+
+  repeatPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Please repeat your password"),
+});
+export const SignInValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required")
+    .email("Please enter an email")
+    .max(254, "Max 254"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(
+      /^(?=.*[A-Z])(?=.*[@$!%*?&])(?=.*[0-9])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain at least one uppercase letter and one special character"
+    ),
+});
